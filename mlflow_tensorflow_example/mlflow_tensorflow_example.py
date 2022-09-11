801dffadea1a353463199df9b26c3532f7f9bf34
yahya_abbas <yahya.abbas@dell.com>
2022-09-11 17:24:52 +0200

    Finish base ML environment image

---
+++ mlflow_tensorflow_example/mlflow_tensorflow_example.py
@@ -0,0 +1,35 @@
+import mlflow
+from mlflow.utils.mlflow_tags import MLFLOW_RUN_NAME
+import mlflow.keras
+
+experiment_name = 'MNIST-Classification'
+mlflow.set_experiment(experiment_name)
+
+from tensorflow import keras
+from tensorflow.keras.datasets import mnist
+
+(X_train, y_train), (X_test, y_test) = mnist.load_data()
+X_train, X_valid = X_train[5000:], X_train[:5000]
+y_train, y_valid = y_train[5000:], y_train[:5000]
+X_train, X_valid = X_train / 255, X_valid / 255
+
+with mlflow.start_run(run_name='three-layers-network'):
+    model = keras.models.Sequential()
+    model.add(keras.layers.Flatten(input_shape=[28, 28]))
+    model.add(keras.layers.Dense(300, activation='relu'))
+    model.add(keras.layers.Dense(300, activation='relu'))
+    model.add(keras.layers.Dense(10, activation='softmax'))
+
+    loss = 'sparse_categorical_crossentropy'
+    optimizer = 'adam'
+    metrics = ['accuracy']
+    epoch = 10
+    batch_size = 128
+
+    model.compile(loss=loss, optimizer=optimizer, metrics=metrics)
+
+    mlflow.keras.autolog()
+
+    model.fit(X_train, y_train, epochs=epoch, batch_size=batch_size, validation_data=(X_valid, y_valid))
+
+    test_loss, test_accuracy = model.evaluate(X_test, y_test)
---
+++ mlflow_torch_example/mlflow_torch_example.py
@@ -0,0 +1,74 @@
+import os
+
+import pytorch_lightning as pl
+import torch
+from torch.nn import functional as F
+from torch.utils.data import DataLoader
+from torchvision import transforms
+from torchvision.datasets import MNIST
+
+try:
+    from torchmetrics.functional import accuracy
+except ImportError:
+    from pytorch_lightning.metrics.functional import accuracy
+
+import mlflow.pytorch
+from mlflow import MlflowClient
+
+# For brevity, here is the simplest most minimal example with just a training
+# loop step, (no validation, no testing). It illustrates how you can use MLflow
+# to auto log parameters, metrics, and models.
+
+class MNISTModel(pl.LightningModule):
+    def __init__(self):
+        super().__init__()
+        self.l1 = torch.nn.Linear(28 * 28, 10)
+
+    def forward(self, x):
+        return torch.relu(self.l1(x.view(x.size(0), -1)))
+
+    def training_step(self, batch, batch_nb):
+        x, y = batch
+        logits = self(x)
+        loss = F.cross_entropy(logits, y)
+        pred = logits.argmax(dim=1)
+        acc = accuracy(pred, y)
+
+        # Use the current of PyTorch logger
+        self.log("train_loss", loss, on_epoch=True)
+        self.log("acc", acc, on_epoch=True)
+        return loss
+
+    def configure_optimizers(self):
+        return torch.optim.Adam(self.parameters(), lr=0.02)
+
+def print_auto_logged_info(r):
+
+    tags = {k: v for k, v in r.data.tags.items() if not k.startswith("mlflow.")}
+    artifacts = [f.path for f in MlflowClient().list_artifacts(r.info.run_id, "model")]
+    print("run_id: {}".format(r.info.run_id))
+    print("artifacts: {}".format(artifacts))
+    print("params: {}".format(r.data.params))
+    print("metrics: {}".format(r.data.metrics))
+    print("tags: {}".format(tags))
+
+# Initialize our model
+mnist_model = MNISTModel()
+
+# Initialize DataLoader from MNIST Dataset
+train_ds = MNIST(os.getcwd(), train=True,
+    download=True, transform=transforms.ToTensor())
+train_loader = DataLoader(train_ds, batch_size=32)
+
+# Initialize a trainer
+trainer = pl.Trainer(max_epochs=5)
+
+# Auto log all MLflow entities
+mlflow.pytorch.autolog()
+
+# Train the model
+with mlflow.start_run() as run:
+    trainer.fit(mnist_model, train_loader)
+
+# fetch the auto logged parameters and metrics
+print_auto_logged_info(mlflow.get_run(run_id=run.info.run_id))
